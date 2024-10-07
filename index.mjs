ff8e0f16d5232fc84cedf42ef109057e4327f7d3
rhtpandeyIN <rht.uimworld@gmail.com>
2024-10-07 10:59:16 +0530

    updating lambda code

--- index.mjs
+++ index.mjs
@@ -2,7 +2,7 @@ export const handler = async (event) => {
     // TODO implement
     const response = {
       statusCode: 200,
-      body: JSON.stringify('Hello from Lambda! Hi from Rohit, this is a test!'),
+      body: JSON.stringify('Hi UU, this is Rohit Pandey, and we are having a session on Serverless Programming'),
     };
     return response;
   };
